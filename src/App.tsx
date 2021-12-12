5ccac0ed7a1c67226269b45f73cea79c8376b02b
matteo9966 <matteoluigitommasi@gmail.com>
2021-12-12 16:19:04 +0100

    created a button variant in the theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,46 @@ import { GridContainer } from "./components/UI/Grid";
 import { GridItem } from "./components/UI/GridItem";
 import { GlobalStyle } from "./GlobalStyle/GlobalStyleComponent";
 import Typography from "@mui/material/Typography";
-
-
-
-
+import { Grid } from "@mui/material";
+import { Button } from "@mui/material";
 
 function App() {
   const theme = useTheme();
 
   return (
     <>
-     {GlobalStyle}
+      {GlobalStyle}
       <GridContainer>
-        <GridItem text="nocturnal Dark" background={theme.palette.nocturnalDark}></GridItem>
-        <GridItem text="Peaceful"  backgroundColor={theme.relaxing.peace}></GridItem>
+        <GridItem
+          text="nocturnal Dark"
+          background={theme.palette.nocturnalDark}
+        ></GridItem>
+        <GridItem
+          text="Peaceful"
+          backgroundColor={theme.relaxing.peace}
+        ></GridItem>
         <GridItem text="Love" backgroundColor={theme.relaxing.love}></GridItem>
-        <GridItem text="Sleep" backgroundColor={theme.relaxing.sleep} ></GridItem>
-        <GridItem text="Danger" backgroundColor={theme.status.danger}></GridItem>
-        <div className="sweet-pink"><Typography variant="h1" >Scritta magenta</Typography></div>  
+        <GridItem
+          text="Sleep"
+          backgroundColor={theme.relaxing.sleep}
+        ></GridItem>
+        <GridItem
+          text="Danger"
+          backgroundColor={theme.status.danger}
+        ></GridItem>
+
+        <Grid item>
+          <div className="sweet-pink">
+            <Typography variant="h1">Scritta magenta</Typography>
+          </div>
+        </Grid>
+        <Grid item>
+          <Button variant="contained">Contained</Button>
+        </Grid>
+        <Grid item>
+          <Button variant="round">Tondo</Button>
+        </Grid>
       </GridContainer>
-  
     </>
   );
 }
--- src/ThemeProvider/ThemeProvider.tsx
+++ src/ThemeProvider/ThemeProvider.tsx
@@ -52,6 +52,12 @@ declare module '@mui/material/styles' {
        nocturnalDark:React.CSSProperties['color']
      }
   }
+  
+  declare module '@mui/material/Button'{
+    interface ButtonPropsVariantOverrides {
+      round:true;
+    }
+  }
 
 export const ThemeGlobalProvider:React.FC = (props) => {
     const theme = createTheme({
@@ -67,6 +73,19 @@ export const ThemeGlobalProvider:React.FC = (props) => {
 
           darkBackground:'linear-gradient(7deg, rgba(184,62,62,0.8267682072829132) 33%, rgba(207,219,62,0.7371323529411764) 41%, rgba(46,170,17,0.7875525210084033) 58%, rgba(77,192,195,0.773546918767507) 63%, rgba(112,84,254,0.7847514005602241) 69%, rgba(168,78,213,0.865983893557423) 72%, rgba(6,160,161,0.835171568627451) 94%)',
           nocturnalDark:'linear-gradient(163deg, rgba(0,0,0,0.8267682072829132) 28%, rgba(84,84,84,0.9640231092436975) 65%, rgba(136,136,136,0.9108018207282913) 100%)'
+        },
+        components:{
+          MuiButton:{styleOverrides:{
+            
+          
+            
+          },
+         variants:[
+           {props:{variant:'round'},style:{
+             borderRadius:'50%',background:'linear-gradient(to right, #0f0c29, #302b63, #24243e)',color:'white',
+           }}
+         ]
+        }
         }
       });
   console.log("inside:")
